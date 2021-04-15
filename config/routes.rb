b51f40e6549f6b4640ad3b371dc63d09ad07c6c6
ORIENTSOFTWARE\thao.nguyenngoc <kundev.rk@gmail.com>
2021-04-15 14:56:53 +0700

    Chapter 11 Account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
-  resources :users
+  scope "(:locale)", locale: /en|vi/ do
+    get '/help', to: 'static_pages#help'
+    get '/about', to: 'static_pages#about'
+    get '/contact', to: 'static_pages#contact'
+    get '/signup', to: 'users#new'
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    resources :users
+    resources :account_activations, only: [:edit]
+  end
 
   def default_url_options
     {locale: I18n.locale}
