fb4be9bcc17b83e75a6ccf3f7b9f0fce295a7479
ORIENTSOFTWARE\thao.nguyenngoc <kundev.rk@gmail.com>
2021-04-15 14:56:53 +0700

    finished chapter6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+<<<<<<< HEAD
     before_save :downcase_email
     validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -11,5 +12,13 @@ class User < ApplicationRecord
     def downcase_email
         email.downcase!
     end
+=======
+    before_save { self.email = email.downcase }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+>>>>>>> 6a17ced (finished chapter6)
 end
 
